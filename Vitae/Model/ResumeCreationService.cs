3428a9fee6fcd5762811b6a07a901a00f5e848b0
Don Hussey <dphseven@gmail.com>
2018-10-05 11:34:35 -0400

    Deleted old ResumeCreationService

--- Vitae/Model/ResumeCreationService.cs
+++ Vitae/Model/ResumeCreationService.cs
@@ -6,7 +6,7 @@
     using System.Linq;
     using System.Windows.Media;
 
-    public class ResumeCreationService
+    public class ResumeCreationServiceXXX
     {
         private WdColor headerColor;
 
