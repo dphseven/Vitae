812abd15561abc23bdd8ed65f2e97119494f6f07
Don Hussey <dphseven@gmail.com>
2018-10-13 10:35:01 -0400

    AddExpertiseVM

---
+++ Vitae/ViewModel/AddExpertiseViewModel.cs
@@ -0,0 +1,95 @@
+﻿namespace Vitae.ViewModel
+{
+    using System;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Windows.Input;
+    using Vitae.Model;
+
+    public class AddExpertiseViewModel : IAddExpertiseViewModel
+    {
+        private IExpertiseRepository repos;
+
+        private UIState formState;
+        private IExpertiseEntity entity;
+
+        public UIState FormState 
+         {
+            get { return formState; }
+            set
+            {
+                formState = value;
+                notifyPropertyChanged();
+            }
+        }
+        public Guid ID 
+        {
+            get { return entity.ID; }
+            set
+            {
+                entity.ID = value;
+                notifyPropertyChanged();
+            }
+        }
+        public string Category 
+        {
+            get { return entity.Category; }
+            set
+            {
+                entity.Category = value;
+                notifyPropertyChanged();
+            }
+        }
+        public string Expertise 
+        {
+            get { return entity.Expertise; }
+            set
+            {
+                entity.Expertise = value;
+                notifyPropertyChanged();
+            }
+        }
+
+        public ICommand AddButtonCmd { get; set; }
+        public ICommand CancelButtonCmd { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+		// Public Methods
+
+        public AddExpertiseViewModel(IExpertiseRepository repository) 
+        {
+            repos = repository;
+        }
+
+		// Private Methods
+
+        private void addExpertiseToRepository() 
+        {
+            repos.Add(entity);
+        }
+
+        private void reset() 
+        {
+            repos = null;
+			id = Guid.Empty;
+			category = null;
+			expertise = null;
+		}
+
+        private void setUpRelayCommands() 
+        {
+            AddButtonCmd = new RelayCommand(
+                T => { addExpertiseToRepository(); reset(); },
+                T => true);
+            CancelButtonCmd = new RelayCommand(
+                T => reset(),
+                T => true);
+        }
+
+        private void notifyPropertyChanged([CallerMemberName] string propertyName = "") 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Vitae/ViewModel/IAddExpertiseViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Vitae.ViewModel
+{
+    using System;
+    using System.ComponentModel;
+    using System.Windows.Input;
+
+    public interface IAddExpertiseViewModel : INotifyPropertyChanged
+    {
+        Guid ID { get; set; }
+        string Category { get; set; }
+        string Expertise { get; set; }
+
+        UIState FormState { get; set; }
+
+        ICommand AddButtonCmd { get; set; }
+        ICommand CancelButtonCmd { get; set; }
+    }
+}
